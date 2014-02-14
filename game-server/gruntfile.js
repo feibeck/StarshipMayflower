34e67153a115cd9a6800070ab5a49bf56e5596a1
Florian Eibeck <florian.eibeck@mayflower.de>
2014-02-14 08:30:09 +0100

    A first test with jasmine

--- game-server/gruntfile.js
+++ game-server/gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+
         jshint: {
             all: ['Gruntfile.js', 'app/**/*.js'],
             options: {
@@ -12,10 +13,25 @@ module.exports = function(grunt) {
                     $: true
                 }
             }
+        },
+
+        jasmine_node: {
+            specNameMatcher: "Spec", // load only specs containing specNameMatcher
+            projectRoot: "app",
+            requirejs: false,
+            forceExit: true,
+            jUnit: {
+                report: false,
+                savePath : "./build/reports/jasmine/",
+                useDotNotation: true,
+                consolidate: true
+            }
         }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jasmine-node');
 
     grunt.registerTask('default', ['jshint']);
 
