a26216c03ba2f09164816e81809b8831e102cfa1
Florian Eibeck <florian.eibeck@mayflower.de>
2014-04-27 22:39:15 +0200

    Fixed roll angle

--- web-server/public/src/Map/directives/ssmCompass.js
+++ web-server/public/src/Map/directives/ssmCompass.js
@@ -27,23 +27,53 @@ define([
                         if (!$scope.ship) {
                             return;
                         }
+
                         compass.jaw(GameUtils.getAngle(-$scope.ship.heading.z, $scope.ship.heading.x));
 
-                        compass.roll(GameUtils.getAngle($scope.ship.shipX.x, $scope.ship.shipX.y));
+                        var ship = $scope.ship;
 
-                        var angle = GameUtils.getAngle($scope.ship.heading.z, $scope.ship.heading.y);
-                        if (angle > 90 && angle <= 180) {
-                            angle = 180 - angle;
-                        }
-                        if (angle > 270 && angle <= 360) {
-                            angle = (360 - angle) * -1;
-                        }
-                        if (angle > 180 && angle <= 270) {
-                            angle = 180 - angle;
-                        }
+                        var rotationMatrix = new THREE.Matrix4(
+                            ship.orientation[0][0],
+                            ship.orientation[0][1],
+                            ship.orientation[0][2],
+                            0,
+                            ship.orientation[1][0],
+                            ship.orientation[1][1],
+                            ship.orientation[1][2],
+                            0,
+                            ship.orientation[2][0],
+                            ship.orientation[2][1],
+                            ship.orientation[2][2],
+                            0,
+                            0,
+                            0,
+                            0,
+                            1
+                        );
 
-                        console.log(angle);
-                        compass.pitch(angle);
+                        var euler = new THREE.Euler(0, 0, 0, 'ZXY');
+                        euler.setFromRotationMatrix(rotationMatrix, 'ZXY');
+
+                        var angleZ = euler.z * (180 / Math.PI);
+                        if (angleZ < 0) {
+                            angleZ = 360 + angleZ;
+                        };
+
+                        angleZ = 360 - angleZ;
+
+                        compass.roll(angleZ);
+
+                        var anglePitch = GameUtils.getAngle($scope.ship.heading.z, $scope.ship.heading.y);
+                        if (anglePitch > 90 && anglePitch <= 180) {
+                            anglePitch = 180 - anglePitch;
+                        }
+                        if (anglePitch > 270 && anglePitch <= 360) {
+                            anglePitch = (360 - anglePitch) * -1;
+                        }
+                        if (anglePitch > 180 && anglePitch <= 270) {
+                            anglePitch = 180 - anglePitch;
+                        }
+                        compass.pitch(anglePitch);
 
                         compass.draw();
 
