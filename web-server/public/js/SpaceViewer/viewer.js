e37ba3f66317b46a63cf943306972f7d39fec857
Florian Eibeck <florian.eibeck@mayflower.de>
2014-05-27 17:32:52 +0200

    SpaceViewer now with rotation

--- web-server/public/js/SpaceViewer/viewer.js
+++ web-server/public/js/SpaceViewer/viewer.js
@@ -9,25 +9,24 @@ define([
     "use strict";
 
     var Viewer = SpaceObjectsRenderer.extend({
+
         initCamera: function() {
             this.camera = new THREE.PerspectiveCamera(
-                45,
+                90,
                 this.width / this.height,
                 0.001,
                 10000
             );
-
-            this.camera.position.set(0, 0.03, -0.075);
-
-            var center = new THREE.Vector3(0, 0.02, 0);
-            this.camera.lookAt(center);
         },
+
         initialize: function() {
             var me = this;
             this.shipModel = null;
             this.renderObjects = {};
         },
+
         drawObjects: function() {
+
             var me = this;
 
             if (this.ship && !this.shipModel) {
@@ -35,9 +34,11 @@ define([
                 if (!this.loading) {
                     this.loading = true;
                     ModelLoader.loadModel(this.ship, function (model) {
-                        model.position.x = 0;
-                        model.position.y = 0;
-                        model.position.z = 0;
+                        model.position = new THREE.Vector3(
+                            0 - model.centeroid.x,
+                            0 - model.centeroid.y,
+                            0 - model.centeroid.z
+                        );
 
                         me.scene.add(model);
                         me.shipModel = model;
@@ -51,68 +52,119 @@ define([
                 return;
             }
 
+            this.placeShip();
+            this.placeCamera();
+
+            _.forIn(this.worldObjects, function(object) {
+                if (me.renderObjects[object.id]) {
+                    me.placeObject(object);
+                } else {
+                    me.loadModel(object);
+                }
+            });
+        },
+
+        placeObject: function(object)
+        {
+            var me = this;
+
+            me.renderObjects[object.id].rotation.setFromRotationMatrix(me.getShipRotationMatrix(object))
+
+            var position = new THREE.Vector3(
+                object.position.x,
+                object.position.y,
+                object.position.z
+            );
+
             var shipPosition = new THREE.Vector3(
                 this.ship.position.x,
                 this.ship.position.y,
                 this.ship.position.z
             );
 
-            _.forIn(this.worldObjects, function(object) {
+            position.sub(shipPosition);
 
-                if (me.renderObjects[object.id]) {
-                    me.renderObjects[object.id].position = me.calculateLocalPosition(shipPosition, object);
-                    if (me.renderObjects[object.id].centeroid) {
-                        me.renderObjects[object.id].position.x -= me.renderObjects[object.id].centeroid.x;
-                        me.renderObjects[object.id].position.y -= me.renderObjects[object.id].centeroid.y;
-                        me.renderObjects[object.id].position.z -= me.renderObjects[object.id].centeroid.z;
-                    }
-                } else {
-                    me.loadObjectSpaceStation(object, object.id);
-                }
+            if (me.renderObjects[object.id].centeroid) {
+                position.x -= me.renderObjects[object.id].centeroid.x;
+                position.y -= me.renderObjects[object.id].centeroid.y;
+                position.z -= me.renderObjects[object.id].centeroid.z;
+            }
 
-            });
+            me.renderObjects[object.id].position = position;
         },
 
-        loadObjectSpaceStation: function(object, objectId)
+        placeShip: function()
+        {
+            var matrix = this.getShipRotationMatrix(this.ship);
+            this.shipModel.rotation.setFromRotationMatrix(matrix);
+        },
+
+        placeCamera: function()
+        {
+            var matrix = this.getShipRotationMatrix(this.ship);
+
+            var position = new THREE.Vector3(
+                0,
+                0,
+                0
+            );
+
+            var direction = new THREE.Vector3(0, 0, 1);
+            direction.applyMatrix4(matrix);
+            direction.setLength(0.03);
+            position.sub(direction);
+
+            var direction = new THREE.Vector3(0, 1, 0);
+            direction.applyMatrix4(matrix);
+            direction.setLength(0.015);
+            position.add(direction);
+
+            this.camera.position = position;
+
+            var cameraMatrix = new THREE.Matrix4(
+                -1, 0, 0,
+                0,
+                0, 1, 0,
+                0,
+                0, 0, -1,
+                0,
+                0,
+                0,
+                0,
+                1
+            );
+
+            matrix.multiply(cameraMatrix);
+
+            this.camera.rotation.setFromRotationMatrix(matrix);
+        },
+
+        loadModel: function(object)
         {
             var me = this;
             me.loading = true;
             ModelLoader.loadModel(object, function(object3d){
                 me.scene.add(object3d);
-                me.renderObjects[objectId] = object3d;
+                me.renderObjects[object.id] = object3d;
                 me.loading = false;
+
                 me.drawObjects();
             });
         },
 
-        calculateLocalPosition: function(shipPosition, object) {
-            var rotation = this.getShipRotationMatrix(),
-                globalPosition = new THREE.Vector3(
-                    object.position.x,
-                    object.position.y,
-                    object.position.z
-                ),
-                localPosition = globalPosition.sub(shipPosition),
-                m1 = new THREE.Matrix4();
-
-            localPosition.applyMatrix4(m1.getInverse(rotation));
-
-            return localPosition;
-        },
-
-        getShipRotationMatrix: function() {
+        getShipRotationMatrix: function(ship) {
             var rotationMatrix = new THREE.Matrix4(
-                this.ship.orientation[0][0],
-                this.ship.orientation[0][1],
-                this.ship.orientation[0][2],
+                ship.orientation[0][0],
+                ship.orientation[0][1],
+                ship.orientation[0][2],
                 0,
-                this.ship.orientation[1][0],
-                this.ship.orientation[1][1],
-                this.ship.orientation[1][2],
+                ship.orientation[1][0],
+                ship.orientation[1][1],
+                ship.orientation[1][2],
                 0,
-                this.ship.orientation[2][0],
-                this.ship.orientation[2][1],
-                this.ship.orientation[2][2],
+                ship.orientation[2][0],
+                ship.orientation[2][1],
+                ship.orientation[2][2],
                 0,
                 0,
                 0,
